7119d5f0809548ef6cd8f6e48e8a7e56c470aa15
Habi12345 <131537314+Habi12345@users.noreply.github.com>
2024-03-18 19:15:57 +0100

    Progress

--- script.js
+++ script.js
@@ -5,17 +5,28 @@ let typed = new Typed(".autotype", {
     loop : true
   });
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
-    });
-});
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+//Scroll Animation
+const animatedElement = document.getElementById('animated-element')
+
+function isElementInlViewport(el){
+    const rect = el.getBoundingClientRect();
+    return(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+
+        &&
+
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function handleScroll() {
+    if(isElementInlViewport(animatedElement)){
+        animatedElement.classList.add('visible');
+        window.removeEventListener('scroll', handleScroll);
+    }
+}
+window.addEventListener('scroll', handleScroll);
