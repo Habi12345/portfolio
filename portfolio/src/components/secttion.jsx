90f9d37bfca2ee9a364b78f1ca572fbdb389cadd
Habib Abdulkareem <131537314+Habi12345@users.noreply.github.com>
2024-05-16 19:57:16 +0100

    More...

--- portfolio/src/components/secttion.jsx
+++ portfolio/src/components/secttion.jsx
@@ -18,7 +18,7 @@ export default function secttion() {
             </div>
             <div className='section'>
                 <span className='dev-header'> <strike>developed</strike> Apps<strong></strong></span>
-                <p>Collection of of my Projects and more projects on my Github 0</p>   
+                <p> Few collection of my Projects and more projects on my <a href="">Github <i class="fa-brands fa-github"></i></a></p>   
             </div>
         </>
     )
