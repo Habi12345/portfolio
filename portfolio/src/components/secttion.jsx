0701ab207ee584b2f0041a99a7ad639020f4fb86
Habib Abdulkareem <131537314+Habi12345@users.noreply.github.com>
2024-05-23 17:58:31 +0100

    ...working

--- portfolio/src/components/secttion.jsx
+++ portfolio/src/components/secttion.jsx
@@ -5,18 +5,45 @@ import devTools from '../components/devTools'
 
 
 export default function secttion() {
-    const showIcons = devTools.map((tool) => (
-        <div data-aos="flip-down" className="dev-Icon" key={tool.id}>
-            <span>{tool.icon}</span>
-            <p>{tool.name}</p>
-        </div>
-    ))
+   // const showIcons = devTools.map((tool) => (
+      //  <div data-aos="flip-down" className="dev-Icon" key={tool.id}>
+         //   <span>{tool.icon}</span>
+         //   <p>{tool.name}</p>
+       // </div>
+   // ))
     return (
         <>
             <div data-aos="fade-down-right" className='section'>
                 <span className='dev-header'> <strike>development</strike> <strong>Tools</strong></span>
                 <div data-aos="zoom-in-up" className='dev-icon-wrapper'>
-                    {showIcons}
+                    <div  data-aos="fade-right" className="dev-icon">
+                        <span><i class="fa-brands fa-html5"></i></span>
+                        <p>HTML</p>
+                    </div>
+                    <div  data-aos="flip-up" className="dev-icon">
+                        <span> <i class="fa-brands fa-css3"></i></span>
+                        <p>CSS</p>
+                    </div>
+                    <div  data-aos="flip-up" className="dev-icon">
+                        <span><i class="fa-brands fa-sass"></i></span>
+                        <p>SASS</p>
+                    </div>
+                    <div  data-aos="fade-left" className="dev-icon">
+                        <span><i class="fa-brands fa-bootstrap"></i></span>
+                        <p>BOOTSTRAP</p>
+                    </div>
+                    <div  data-aos="fade-up-right" className="dev-icon">
+                        <span><i class="fa-brands fa-js"></i></span>
+                        <p>JAVASCRIPT</p>
+                    </div>
+                    <div  data-aos="flip-up" className="dev-icon">
+                        <span><i class="fa-brands fa-react"></i></span>
+                        <p>REACT</p>
+                    </div>
+                    <div  data-aos="fade-up-left" className="dev-icon">
+                        <span><i class="fa-brands fa-git-alt"></i></span>
+                        <p>GIT</p>
+                    </div>
                 </div>
             </div>
             <div data-aos="fade-up-left" className='section'>
